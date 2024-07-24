9a4cf400b2fb7a440e9a2125da324ee42c66ca6a
hoangnh4204 <hoangnh4204@gmail.com>
2024-07-24 22:46:41 +0700

    update Maximum number of classes registered

---
+++ project-base/src/main/java/com/example/projectbase/constant/ValidationErrorMessages.java
@@ -0,0 +1,6 @@
+package com.example.projectbase.constant;
+
+public class ValidationErrorMessages {
+    public static final String COURSE_ALREADY_REGISTERED = "Course has already been registered by this member.";
+    public static final String MAX_COURSES_REGISTERED = "Member has already registered for the maximum number of allowed courses.";
+}
