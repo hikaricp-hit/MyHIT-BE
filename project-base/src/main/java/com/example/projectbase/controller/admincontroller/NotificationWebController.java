648770b47ee75679beb102dd9df3b059e0f2ba9e
hoangnh4204 <hoangnh4204@gmail.com>
2024-08-09 14:38:09 +0700

    update web admin page

--- project-base/src/main/java/com/example/projectbase/controller/admincontroller/NotificationWebController.java
+++ project-base/src/main/java/com/example/projectbase/controller/admincontroller/NotificationWebController.java
@@ -1,12 +1,54 @@
-package com.example.projectbase.controller.admincontroller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class NotificationWebController {
-    @GetMapping("/auth/notification")
-    public String success() {
-        return "pages-notification";
-    }
-}
+//package com.example.projectbase.controller.admincontroller;
+//
+//import com.example.projectbase.domain.dto.pagination.PaginationFullRequestDto;
+//import com.example.projectbase.domain.dto.pagination.PaginationResponseDto;
+//import com.example.projectbase.domain.dto.request.MemberCreateDto;
+//import com.example.projectbase.domain.dto.request.MemberUpdateDto;
+//import com.example.projectbase.domain.dto.request.NotificationCreateDto;
+//import com.example.projectbase.domain.entity.Member;
+//import com.example.projectbase.domain.entity.Notification;
+//import com.example.projectbase.service.NotificationService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@RestController
+//@RequestMapping("/auth/notification")
+//public class NotificationWebController {
+//
+//    @Autowired
+//    NotificationService notificationService;
+//
+//    @GetMapping("")
+//    public ModelAndView success(Model model) {
+//        PaginationResponseDto<Notification> list = notificationService.getAll( new PaginationFullRequestDto("","",true,0,1000));
+//        model.addAttribute("list", list.getItems());
+//        return new ModelAndView("pages-notification");
+//    }
+//    @PostMapping("/create")
+//    public ModelAndView create(@ModelAttribute NotificationCreateDto notificationCreateDto, Model model) {
+//        notificationService.createMember(notificationCreateDto);
+//        PaginationResponseDto<Notification> list = notificationService.getAll( new PaginationFullRequestDto("","",true,0,1000));
+//        model.addAttribute("respone", "Create notification success!");
+//        model.addAttribute("list", list.getItems());
+//        return new ModelAndView("pages-notification");
+//    }
+//    @PostMapping("/update")
+//    public ModelAndView update(@RequestParam String memberUpdateId, @ModelAttribute MemberUpdateDto memberUpdateDto, Model model) {
+//        notificationService.updateMember(memberUpdateId,memberUpdateDto);
+//        PaginationResponseDto<Notification> list = notificationService.getAll( new PaginationFullRequestDto("","",true,0,1000));
+//        model.addAttribute("respone", "Update notification success!");
+//        model.addAttribute("list", list.getItems());
+//        return new ModelAndView("pages-notification");
+//    }
+//    @PostMapping("/delete")
+//    public ModelAndView delete(@RequestParam String memberDeleteId, Model model) {
+//        notificationService.deleteMember(memberDeleteId);
+//        PaginationResponseDto<Notification> list = notificationService.getAll( new PaginationFullRequestDto("","",true,0,1000));
+//        model.addAttribute("respone", "Delete notification success!");
+//        model.addAttribute("list", list.getItems());
+//        return new ModelAndView("pages-notification");
+//    }
+//}
--- project-base/src/main/java/com/example/projectbase/controller/admincontroller/RegisterWebController.java
+++ project-base/src/main/java/com/example/projectbase/controller/admincontroller/RegisterWebController.java
@@ -1,12 +1,57 @@
 package com.example.projectbase.controller.admincontroller;
 
+import com.example.projectbase.domain.dto.pagination.PaginationFullRequestDto;
+import com.example.projectbase.domain.dto.pagination.PaginationResponseDto;
+import com.example.projectbase.domain.dto.request.MemberCreateDto;
+import com.example.projectbase.domain.dto.request.MemberUpdateDto;
+import com.example.projectbase.domain.entity.Member;
+import com.example.projectbase.domain.entity.Register;
+import com.example.projectbase.service.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
+@RequestMapping("/auth/register")
 public class RegisterWebController {
-    @GetMapping("/auth/register")
-    public String success() {
-        return "pages-register";
+
+    @Autowired
+    RegisterService registerService;
+
+    @GetMapping("")
+    public ModelAndView success(Model model) {
+        PaginationResponseDto<Register> list = registerService.getAllRegisters( new PaginationFullRequestDto("","",true,0,1000));
+        model.addAttribute("list", list.getItems());
+        return new ModelAndView("pages-register");
+    }
+
+    @PostMapping("/accept")
+    public ModelAndView accept(@RequestParam String acceptRegisterId, Model model) {
+        registerService.acceptRegister(acceptRegisterId);
+        PaginationResponseDto<Register> list = registerService.getAllRegisters( new PaginationFullRequestDto("","",true,0,1000));
+        model.addAttribute("respone", "Register accepted!");
+        model.addAttribute("list", list.getItems());
+        return new ModelAndView("pages-register");
+    }
+
+    @PostMapping("/reject")
+    public ModelAndView reject(@RequestParam String rejectRegisterId, Model model) {
+        registerService.rejectRegister(rejectRegisterId);
+        PaginationResponseDto<Register> list = registerService.getAllRegisters( new PaginationFullRequestDto("","",true,0,1000));
+        model.addAttribute("respone", "Register was rejected!");
+        model.addAttribute("list", list.getItems());
+        return new ModelAndView("pages-register");
+    }
+
+
+    @PostMapping("/delete")
+    public ModelAndView delete(@RequestParam String registerDeleteId, Model model) {
+        registerService.deleteRegister(registerDeleteId);
+        PaginationResponseDto<Register> list = registerService.getAllRegisters( new PaginationFullRequestDto("","",true,0,1000));
+        model.addAttribute("respone", "Delete register success!");
+        model.addAttribute("list", list.getItems());
+        return new ModelAndView("pages-register");
     }
 }
